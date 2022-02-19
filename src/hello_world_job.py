eac22ac499046e7d5d50a7d76ea6ab1262007e91
Bastian.hundt <bastian@hundt.me>
2022-02-19 12:37:42 +0100

    playing around

--- src/hello_world_job.py
+++ src/hello_world_job.py
@@ -1,10 +1,12 @@
 import os
-from dagster import job, op, ScheduleDefinition, DefaultScheduleStatus
+from dagster import job, op
+from dagster.utils import file_relative_path
 
 @op
 def hello():
     print("Hello World!")
     print(os.getcwd())
+    print(file_relative_path(__file__, './environments/schedules.yaml'))
 
 @job
 def hello_world_job():

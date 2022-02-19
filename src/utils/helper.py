2cf1f8435406dcc391c1ef406d5fc58254bcf37e
Bastian.hundt <bastian@hundt.me>
2022-02-19 20:30:34 +0100

    need to uniquely name jobs

--- src/utils/helper.py
+++ src/utils/helper.py
@@ -1,5 +1,6 @@
 from dagster import graph
+from config import get_environment_config
 
 def make_job_from_graph(graph):
-	env = utils.get_config()['env']
+	env = utils.get_environment_config()['env']
 	return graph.to_job(name=graph.__name__ + "_" + env)
